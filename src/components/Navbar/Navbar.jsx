577f58cfa326f64de2b7178f6c9aa069329a5ce9
fansters <edmundseizentals@gmail.com>
2022-09-01 14:48:06 +0300

    creating navbar for desktop and mobile. Now creating mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,48 @@
 import React from "react";
+
+import { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const [toggle, setToggle] = useState(true);
+
+	return (
+		<nav className='app__navbar'>
+			<div className='app__navbar-logo'>
+				<img src={images.logo} alt='logo' />
+			</div>
+			{/* Desktop menu */}
+			<ul className='app__navbar-links'>
+				{["home", "about", "work", "skills", "contact"].map((item) => (
+					<li className='app__flex p-text' key={`link-${item}`}>
+						<div id='navbar__div' />
+						<a href={`#${item}`}>{item}</a>
+						<div id='navbar__divv' />
+					</li>
+				))}
+			</ul>
+
+			{/* Burger menu */}
+			<div className='app__navbar-menu'>
+				<HiMenuAlt4 onClick={() => setToggle(true)} />
+				{toggle && (
+               <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: "easeOut" }}>
+                  <HiX onClick={() => setToggle(false)} />
+                  {["home", "about", "work", "skills", "contact"].map((item) => (
+					<li key={item}>
+						{/* <div id='navbar__div' /> */}
+						<a href={  `#${item}`} onClick={() => setToggle(false)}>{item}</a>
+						{/* <div id='navbar__divv' /> */}
+					</li>
+				))}
+               </motion.div>
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
