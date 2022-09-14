26f33b117937741393d59d4d575a329492707add
fansters <edmundseizentals@gmail.com>
2022-09-14 14:59:11 +0300

    working on skills section. Trying to figure out why content dissapeard on website

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -7,8 +7,8 @@ import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
-const [experience, setExperience] = useState([])
-const [skills , setSkills  ] = useState([])
+	const [experience, setExperience] = useState([]);
+	const [skills, setSkills] = useState([]);
 
 	useEffect(() => {
 		const query = '*[_type == "experiences"]';
@@ -26,20 +26,42 @@ const [skills , setSkills  ] = useState([])
 		<h2 className='head-text'>skillset & exp</h2>
 
 		<div className='app__skills-container'>
-         <motion.div className='app__skills-list'>{skills.map((skills) => (
-            <motion.div
-               whileInView={{ opacity: [0, 1] }}
-               transition={{ duration: 0.5 }}
-               className='app__skills-item app__flex'
-               key={skills.name}
-            >
-               <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
-                  <img src={urlFor(skill.icon)} alt={skill.name} />
-               </div>
-               <p className='p-text'>{skill.name}</p>
+			<motion.div className='app__skills-list'>
+				{skills.map((skill) => (
+					<motion.div
+						whileInView={{ opacity: [0, 1] }}
+						transition={{ duration: 0.5 }}
+						className='app__skills-item app__flex'
+						key={skill.name}
+					>
+						<div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
+							<img src={urlFor(skill.icon)} alt={skill.name} />
+						</div>
+						<p className='p-text'>{skill.name}</p>
+					</motion.div>
+				))}
+			</motion.div>
 
-            </motion.div>
-         ))}</motion.div>
+			<motion.div className='app__skills-exp'>
+				{experience.works.map((work) => (
+					<>
+						<motion.div
+							whileInView={{ opacity: [0, 1] }}
+							transition={{ duration: 0.5 }}
+							className='app__skills-exp-work'
+							data-tip
+							data-for={work.name}
+							key={work.name}
+						>
+							<h4 className='bold-text'>{work.name}</h4>
+							<p className='p-text'>{work.company}</p>
+						</motion.div>
+						<ReactTooltip id={work.name} effect='solid' arrowColor='#fff' className='skills-tooltip'>
+							{work.desc}
+						</ReactTooltip>
+					</>
+				))}
+			</motion.div>
 		</div>
 	</>;
 };
