683041c9f97672c54832d7689cd89d3372dacab2
fansters <edmundseizentals@gmail.com>
2022-10-04 16:15:43 +0300

    added new works. added new bg images, svg elemtns. updated skills. updated new works. need to do small mobile changes

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
 			</h2>
 
 			<div className='app__work-filter'>
-				{["UI/UX", "Web App", "Mobile App", "React JS", "All"].map((item, index) => (
+				{["All", "JS", "API", "SCSS", "CSS", "Wordpress", "ReactJS"].map((item, index) => (
 					<div
 						key={index}
 						onClick={() => handleWorkFilter(item)}
