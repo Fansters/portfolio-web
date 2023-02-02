4d4f2a14356069349e976bbae562b3f7eba7d856
fansters <edmundseizentals@gmail.com>
2023-02-02 15:35:22 +0200

    redesigning website. changed work section. need t add diff content  at about page

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -33,6 +33,79 @@ const Footer = () => {
 		});
 	};
 
+	// FORM VALIDATION
+	class Test extends React.Component {
+		constructor(props) {
+			super(props);
+
+			this.state = {
+				fields: {},
+				errors: {},
+			};
+		}
+
+		handleValidation() {
+			let fields = this.state.fields;
+			let errors = {};
+			let formIsValid = true;
+
+			//Name
+			if (!fields["name"]) {
+				formIsValid = false;
+				errors["name"] = "Cannot be empty";
+			}
+
+			if (typeof fields["name"] !== "undefined") {
+				if (!fields["name"].match(/^[a-zA-Z]+$/)) {
+					formIsValid = false;
+					errors["name"] = "Only letters";
+				}
+			}
+
+			//Email
+			if (!fields["email"]) {
+				formIsValid = false;
+				errors["email"] = "Cannot be empty";
+			}
+
+			if (typeof fields["email"] !== "undefined") {
+				let lastAtPos = fields["email"].lastIndexOf("@");
+				let lastDotPos = fields["email"].lastIndexOf(".");
+
+				if (
+					!(
+						lastAtPos < lastDotPos &&
+						lastAtPos > 0 &&
+						fields["email"].indexOf("@@") == -1 &&
+						lastDotPos > 2 &&
+						fields["email"].length - lastDotPos > 2
+					)
+				) {
+					formIsValid = false;
+					errors["email"] = "Email is not valid";
+				}
+			}
+
+			this.setState({ errors: errors });
+			return formIsValid;
+		}
+
+		contactSubmit(e) {
+			e.preventDefault();
+
+			if (this.handleValidation()) {
+				alert("Form submitted");
+			} else {
+				alert("Form has errors.");
+			}
+		}
+
+		handleChange(field, e) {
+			let fields = this.state.fields;
+			fields[field] = e.target.value;
+			this.setState({ fields });
+		}
+	}
 	return (
 		<>
 			<h2 className='head-text'>Sazinies ar mani!</h2>
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -36,6 +36,20 @@ const Work = () => {
 		}, 500);
 	};
 
+	const handleMouseMove = (e) => {
+		const { currentTarget: target } = e;
+		const rect = target.getBoundingClientRect(),
+			x = e.clientX - rect.left,
+			y = e.clientY - rect.top;
+
+		target.style.setProperty("--mouse-x", `${x}px`);
+		target.style.setProperty("--mouse-y", `${y}px`);
+	};
+
+	for (const card of document.querySelectorAll("card")) {
+		card.onmousemove = (e) => handleMouseMove(e);
+	}
+
 	return (
 		<>
 			<h2 className='head-text'>
