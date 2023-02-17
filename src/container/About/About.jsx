bce8676aa05f0ed595dfe9d906efeab688db8d47
fansters <edmundseizentals@gmail.com>
2023-02-17 19:31:12 +0200

    wokred on img in photoshop. adde new img. changed card styles and html

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -27,46 +27,43 @@ const About = () => {
 			<section className='hero-section'>
 				<div className='card-grid'>
 					<a className='card'>
-						<div className='card__background' />
+						<div className='card__background planning' />
 						<div className='card__content'>
-							<p className='card__category'>Planning</p>
-							<h3 className='card__heading'>Exploring your needs</h3>
+							<h3 className='card__heading'>Planning</h3>
+							<p className='card__category'>Exploring your needs</p>
+						</div>
+                  <div className='card__numbers'>
+							<p className='card__number'>1</p>
 						</div>
 					</a>
 					<a className='card'>
-						<div
-							className='card__background'
-							style={{
-								backgroundImage: `url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)`,
-							}}
-						/>
+						<div className='card__background design' />
 						<div className='card__content'>
-							<p className='card__category'>Design</p>
-							<h3 className='card__heading'>Composing the gathared ideas</h3>
+							<h3 className='card__heading'>Design</h3>
+							<p className='card__category'>Composing the gathared ideas</p>
+						</div>
+						<div className='card__numbers'>
+							<p className='card__number'>2</p>
 						</div>
 					</a>
 					<a className='card'>
-						<div
-							className='card__background'
-							style={{
-								backgroundImage: `url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)`,
-							}}
-						/>
+						<div className='card__background program' />
 						<div className='card__content'>
-							<p className='card__category'>Creation</p>
-							<h3 className='card__heading'>Websites comes to life.</h3>
+							<h3 className='card__heading'>Creation</h3>
+							<p className='card__category'>Websites comes to life</p>
+						</div>
+                  <div className='card__numbers'>
+							<p className='card__number'>3</p>
 						</div>
 					</a>
 					<a className='card'>
-						<div
-							className='card__background'
-							style={{
-								backgroundImage: `url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)`,
-							}}
-						/>
+						<div className='card__background final' />
 						<div className='card__content'>
-							<p className='card__category'>Finish</p>
-							<h3 className='card__heading'>Final touches and future maintenance </h3>
+							<h3 className='card__heading'>Finish</h3>
+							<p className='card__category'>Final touches and future maintenance </p>
+						</div>
+                  <div className='card__numbers'>
+							<p className='card__number'>4</p>
 						</div>
 					</a>
 				</div>
