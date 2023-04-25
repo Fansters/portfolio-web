4713a3ff2afe9298e6adc71a56be93b7f9e051eb
fansters <edmundseizentals@gmail.com>
2023-04-25 22:00:24 +0300

    fixed nav burger menu anims nad changed to dark theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import { HiMenuAlt4, HiX } from "react-icons/hi";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { images } from "../../constants";
 import "./Navbar.scss";
 
 const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
-
-	// const navbar = document.querySelector(".app__navbar");
-	// window.onscroll = () => {
-	// 	if (window.scrollY > 350) {
-	// 		navbar.classList.add("nav-active");
-	// 	} else {
-	// 		navbar.classList.remove("nav-active");
-	// 	}
-	// };
-
-	// const handleClick = (event) => {
-	// 	// event.currentTarget.style.backgroundColor = "salmon";
-	// 	event.currentTarget.style.color = "var(--secondary-color)";
-
-	// 	// event.currentTarget.classList.add("my-class-1", "my-class-2");
-	// };
 
 	return (
 		<nav className='app__navbar'>
@@ -51,20 +35,28 @@ const Navbar = () => {
 			{/* Burger menu */}
 			<div className='app__navbar-menu'>
 				<HiMenuAlt4 onClick={() => setToggle(true)} />
-				{toggle && (
-					<motion.div whileInView={{ x: [400, 0] }} transition={{ duration: 0.85, ease: "easeInOut" }}>
-						<HiX onClick={() => setToggle(false)} />
-						<ul>
-							{["home", "about", "work", "skills", "contact"].map((item) => (
-								<li key={item}>
-									<a href={`#${item}`} onClick={() => setToggle(false)}>
-										{item}
-									</a>
-								</li>
-							))}
-						</ul>
-					</motion.div>
-				)}
+				<AnimatePresence>
+					{toggle && (
+						<motion.div
+							initial={{ x: 900 }}
+							animate={{ x: 0 }}
+							exit={{ x: 900 }}
+							transition={{ duration: 0.75, ease: "easeInOut" }}
+							key='burger-menu'
+						>
+							<HiX onClick={() => setToggle(false)} />
+							<ul>
+								{["home", "about", "work", "skills", "contact"].map((item) => (
+									<li key={item}>
+										<a href={`#${item}`} onClick={() => setToggle(false)}>
+											{item}
+										</a>
+									</li>
+								))}
+							</ul>
+						</motion.div>
+					)}
+				</AnimatePresence>
 			</div>
 		</nav>
 	);
